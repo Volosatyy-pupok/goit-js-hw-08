f89673a35f6641e88496c968cc8bd29dc59cb434
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2023-08-20 14:41:33 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = new SimpleLightbox(".gallery a");
+});
 console.log(galleryItems);
