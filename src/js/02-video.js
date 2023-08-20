f0c7f93ffc867d7408764a9388d4aa395a8e23fd
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2023-08-20 15:01:20 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,45 +1,14 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Vimeo.Player(document.getElementById('vimeo-player'));
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-const localStorageKey = 'videoplayer-current-time';
+const CURRENT_TIME_KEY = `video-player-timekey`;
+function getCurrentTimeKey(timeKey){
+    localStorage.setItem(CURRENT_TIME_KEY, timeKey.seconds);
+}
+player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
 
-// Function to save the current playback time to local storage
-const savePlaybackTime = (time) => {
-  localStorage.setItem(localStorageKey, time);
-};
-
-// Function to load the saved playback time from local storage
-const loadPlaybackTime = () => {
-  const savedTime = localStorage.getItem(localStorageKey);
-  return savedTime ? parseFloat(savedTime) : 0;
-};
-
-// Initialize the player with the saved playback time (if available)
-const initializePlayer = () => {
-  const savedTime = loadPlaybackTime();
-
-  if (savedTime > 0) {
-    player.setCurrentTime(savedTime).catch((error) => {
-      console.error('Error setting current time:', error);
-    });
-  }
-
-  // Start playing the video if savedTime is not available
-  player.play();
-};
-
-// Throttle the saving of playback time to once per second
-const savePlaybackTimeThrottled = throttle((time) => {
-  savePlaybackTime(time);
-}, 1000);
-
-// Listen for the 'timeupdate' event and save the playback time to local storage
-player.on('timeupdate', (data) => {
-  const currentTime = data.seconds;
-  savePlaybackTimeThrottled(currentTime);
-});
-
-// Initialize the player with the saved playback time
-initializePlayer();
+const currentTimeValue = localStorage.getItem(CURRENT_TIME_KEY);
+player.setCurrentTime(currentTimeValue || 0);
